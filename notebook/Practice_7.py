f45ff8f4441737dab91a4455c141faa05b3ce6e1
jaison080 <jaisondennis080@gmail.com>
2022-04-15 23:07:02 +0530

    Updated files

---
+++ notebook/Practice_7.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def calculate_triangle_area(base,height):
+    return 1/2*(base*height)
+
+
+# In[2]:
+
+
+def calculate_square_area(length):
+    return length*length
+
+
+# In[ ]:
+
+
+
+
